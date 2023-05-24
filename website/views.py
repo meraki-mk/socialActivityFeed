85f6ff48112292246b8d1996eed7ec64ad52c0bb
Meenakshi Kharel <meenakshi.kharel23@gmail.com>
2023-05-24 20:12:34 +0530

    like feature added

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, jsonify
 from flask_login import login_required, current_user
-from .models import Post, User
+from .models import Post, User, Like
 from . import db
 
 views = Blueprint("views", __name__)
@@ -60,3 +60,22 @@ def posts(username):
 
     posts = user.posts
     return render_template("posts.html", user=current_user, posts=posts, username=username)
+
+@views.route("/like-post/<post_id>", methods=['GET'])
+@login_required
+def like(post_id):
+    post = Post.query.filter_by(id=post_id).first()
+    like = Like.query.filter_by(
+        author=current_user.id, post_id=post_id).first()
+
+    if not post:
+        return jsonify({'error': 'Post does not exist.'}, 400)
+    elif like:
+        db.session.delete(like)
+        db.session.commit()
+    else:
+        like = Like(author=current_user.id, post_id=post_id)
+        db.session.add(like)
+        db.session.commit()
+
+    return jsonify({"likes": len(post.likes), "liked": current_user.id in map(lambda x: x.author, post.likes)})
